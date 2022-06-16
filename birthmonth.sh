08eb2f15f7f4187e4b5f954656204bf3adc291d1
sagarbachhav1995 <107263157+sagarbachhav1995@users.noreply.github.com>
2022-06-16 22:51:11 +0530

    Add files via upload

---
+++ birthmonth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+no_of_individuals=50
+declare -A birth_day
+echo "The birthday month and year of $no_of_individuals individuals are: "
+for(( individual = 1; individual <= no_of_individuals; individual++ ))
+do
+    (( birth_month = RANDOM % 12 + 1 ))
+    (( birth_year = RANDOM % 2 + 92 ))
+    echo "individual $individual has birthday in month $birth_month year $birth_year "
+    #individual is added to the individual list
+    #which contains list of individuals
+    #in which people have birthday in given month
+    birth_day[$birth_month]+=" ${individual}"
+done
